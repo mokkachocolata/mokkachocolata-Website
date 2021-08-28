ef33248884323b6c4f04b971980ad542eccceaf2
mokkachocolata <89687960+mokkachocolata@users.noreply.github.com>
2021-08-28 17:49:18 +0700

    JS File

---
+++ isdebug.js
@@ -0,0 +1,7 @@
+var isDebug = false;
+if (isDebug == true) {
+var x = document.createElement("A");
+  var t = document.createTextNode("Debug is on");
+  x.appendChild(t);
+  document.body.appendChild(x);
+}
